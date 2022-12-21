25529eaec3c7936ad7eb9556123b407332870cab
AndrewTPascal <andrvotkinski@yandex.ru>
2022-12-21 23:34:04 +0400

    Добавил проект: степень числа через рекурсию

---
+++ ConsoleApp_Task_5_PowerUp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp_Task_5_PowerUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Введите степень числа {x}: ");
+            int pow = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{x} в степени {pow} = {PowerUp(x, pow)}");
+        }
+
+
+        private static int PowerUp(int N, int pow)
+        {
+            if (pow > 0)
+            {
+                 return N * PowerUp(N, --pow);
+            }
+            else
+                return 1;
+         }
+
+    }
+}
