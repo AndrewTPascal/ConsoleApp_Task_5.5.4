5c57aa21cb3ee3bf489a863fc6e925a45a09b3cf
AndrewTPascal <andrvotkinski@yandex.ru>
2022-12-21 22:05:56 +0400

    Добавил изменение цвета фона строки

--- ConsoleApp_Task_5.5.4/Program.cs
+++ ConsoleApp_Task_5.5.4/Program.cs
@@ -17,6 +17,45 @@ namespace ConsoleApp_Task_5._5._4
             Console.ReadKey();
         }
 
+        static void color(int deep)
+        {
+            switch (deep)
+            {
+                case 1:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                                          
+                    break;
+
+                case 2:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                                         
+                    break;
+
+                case 3:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    
+                                        
+                    break;
+                
+                case 4:
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+ 
+                    break;
+
+                case 5:
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+
+                    break;
+
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                                        
+                    break;
+            }
+
+        }
+
         static void Echo(string saidworld, int deep)
         {
             var modif = saidworld;
@@ -26,6 +65,8 @@ namespace ConsoleApp_Task_5._5._4
                 modif = modif.Remove(0, 2);
             }
 
+            color(deep);
+            
             Console.WriteLine("..." + modif);
 
             if (deep > 1)
