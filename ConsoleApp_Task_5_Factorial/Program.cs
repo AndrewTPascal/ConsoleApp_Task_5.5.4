f98d04e593fca034e7b660cff3512524c1408574
AndrewTPascal <andrvotkinski@yandex.ru>
2022-12-21 22:21:22 +0400

    Добавил проект: факториал числа

---
+++ ConsoleApp_Task_5_Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp_Task_5_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{x}! = {Factorial(x)}") ;
+        }
+
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+    }
+}
